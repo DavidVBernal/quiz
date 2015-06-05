e8fad65e6077de64931168c4ad40fa41d052218d
DavidVBernal <davidv.bernal@hotmail.com>
2015-06-05 17:04:17 +0200

    Validaciones

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -7,9 +7,9 @@ exports.load = function(req, res, next, quizId) {
 			if (quiz) {
 				req.quiz = quiz;
 				next();
-			} else { next (new Error ('No existe quizId=' + quizId));}
+			} else { next (new Error ('No existe quizId=' + quizId))}
 		}
-	).catch(function (error) {next (error);});
+	).catch(function (error) {next (error)});
 };
 
 // GET /quizes
@@ -22,21 +22,21 @@ exports.index = function (req, res) {
 
 		models.Quiz.findAll({where: ["pregunta like ?", find]}).then(
 			function (quizes) {
-				res.render('quizes/results', {quizes : quizes});
+				res.render('quizes/results.ejs', {quizes : quizes, errors: []});
 			}
 		).catch(function(error){next(error);})
 	} else {
 		models.Quiz.findAll().then(
 			function (quizes) {
-				res.render('quizes/index', {quizes : quizes});
+				res.render('quizes/index.ejs', {quizes : quizes, errors: []});
 			}
-		).catch(function(error){next(error);})
+		).catch(function(error){next(error)})
 	}
 };
 
 // GET /quizes/:id
 exports.show = function (req, res){
-	res.render('quizes/show', {quiz : req.quiz});
+	res.render('quizes/show', {quiz : req.quiz, errors: []});
 };
 
 // GET /quizes/question
@@ -57,7 +57,7 @@ exports.answer = function (req, res) {
 			var resultado = 'Correcto';
 		}
 
-		res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+		res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado, errors: []});
 		//})
 };
 
@@ -68,20 +68,35 @@ exports.new = function(req, res) {
 		{pregunta: "Pregunta", respuesta :"Respuesta"}
 	);
 
-	res.render('quizes/new', {quiz: quiz});
+	res.render('quizes/new', {quiz: quiz, errors: []});
 };
 
 // POST /quizes/create
 exports.create = function (req, res) {
-	var quiz = models.Quiz.build(req.body.Quiz);
+	var quiz= models.Quiz.build(req.body.quiz);
+	
+	var errors = quiz.validate();
+	if (errors)
+	{
+		/*
+		for (var prop in errors) {
+			if (errors.hasOwnProperty(prop))
+			   console.log("Errors for field " + prop + ": ");
+		    for (var i=0; i<errors[prop].length; ++i) {
+			    console.log("\t" + errors[prop][i]);
+			}
+		}
+		*/
+	    res.render('quizes/new', {quiz: quiz, errors: errors});
+	} else {
+		quiz // save: guarda en DB campos pregunta y respuesta de quiz
+			.save({fields: ["pregunta", "respuesta"]})
+	        .then( function(){ res.redirect('/quizes')}) ;	
+	}
 
-	//guarda en DB los campos pregunta y respuesta de Quiz
-	quiz.save({fields: ["pregunta", "respuesta"]}).then(function() {
-		res.redirect('/quizes');
-	})
 };
 
 // GET /autor
 exports.autor = function (req, res) {
-	res.render('autor', {autor: 'David Velazquez Bernal'});
+	res.render('autor', {autor: 'David Velazquez Bernal', errors: []});
 };
